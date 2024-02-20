55874a77fc8e4b9e7c044352f728c213c96ff210
abinayacad01 <abinayakumarcad01@gmail.com>
2024-02-20 18:06:57 +0530

    modified

--- Program.cs
+++ Program.cs
@@ -60,44 +60,96 @@
 // }
 
 
-class Abi{
-    static void Main(string [] args){
-        Console.WriteLine("added value is");
-        Beaut her = new Beaut();
-        her.g();
-        her.booboo();
+// class Abi{
+//     static void Main(string [] args){
+//         Console.WriteLine("added value is");
+//         Beaut her = new Beaut();
+//         her.g();
+//         her.booboo();
 
-    }
-}
+//     }
+// }
 
-public class Beaut{
-    int a=20;
-    int b=90;
-    int c;
+// public class Beaut{
+//     int a=20;
+//     int b=90;
+//     int c;
 
-    public Beaut(){
-        c = a + b;
-    }
- public void g(){
-        Console.WriteLine(c);
-        if (c>100){
-          Console.WriteLine("its greater than 100");
+//     public Beaut(){
+//         c = a + b;
+//     }
+//  public void g(){
+//         Console.WriteLine(c);
+//         if (c>100){
+//           Console.WriteLine("its greater than 100");
+//         }
+//         else{
+//           Console.WriteLine("its less than 100");
+//         }
+        
+//     }
+
+// public void booboo(){
+//     do{
+//         Console.WriteLine(c);
+//         c++;
+//     }
+//     while(c<120);
+// }
+// }
+
+class arithmetic{
+     static void Main(string [] args){
+         Function aa = new Function();
+         aa.ll();
+         aa.mm();
+         aa.oo();
+     }
+ }
+
+ public class Function {
+     public void ll(){
+     int a=20;
+     int b=10;
+     int add=a+b;
+     Console.WriteLine("added value is "+add);
+     int sub=a-b;
+     Console.WriteLine("subracted value is "+sub);
+     int mul=a*b;
+     Console.WriteLine("product value is "+mul);
+     int div=a/b;
+     Console.WriteLine("divided value is "+div);
+     int mod=a%b;
+     Console.WriteLine("modulus value is "+mod);
+ }
+
+ public void mm(){
+    int x=100;
+    int y=110;
+    if(x==y){
+        Console.WriteLine("x is equals to y");
+     }
+    else{
+        if(x>y){
+          Console.WriteLine("x is greater than y");  
+        }
+
+        else if(y>x){
+            Console.WriteLine("x is less than y");
         }
         else{
-          Console.WriteLine("its less than 100");
+            Console.WriteLine("x is not equals to y");
         }
-        
     }
+ }
 
-public void booboo(){
-    do{
-        Console.WriteLine(c);
-        c++;
-    }
-    while(c<120);
-}
-}
+ public void oo(){
+    var l=78;
+var output1=(40<=l && l<=69)? "wrong" :(76<=l && l<=100) ? "correct" : "none";
+Console.WriteLine(output1);
 
+}
+ }
 
 
 
