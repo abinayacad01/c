c2bb3a25de85fcb067a649c784f6ad9bfcd96898
abinayacad01 <abinayakumarcad01@gmail.com>
2024-02-20 14:33:09 +0530

    modified

---
+++ Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+
+// class Abi{
+//     static void Main(string [] args){
+//         Console.WriteLine("added value is");
+//         Beaut her = new Beaut();
+//         her.g();
+
+//     }
+// }
+
+// public class Beaut{
+//     int a=20;
+//     int b=90;
+//     int c;
+
+//     public Beaut(){
+//         c = a + b;
+//     }
+//  public void g(){
+//         Console.WriteLine(c);
+//         if (c>100){
+//           Console.WriteLine("its greater than 100");
+//         }
+//         else{
+//           Console.WriteLine("its less than 100");
+//         }
+        
+//     }
+// }
+
+
+
+//using string
+
+
+// class Abu{
+
+//   static void Main(string [] args) {
+//     Console.WriteLine("abu is a");
+//     Stoic tt=new Stoic();
+//     tt.pers();
+//     tt.pers2();
+
+//   }
+// }
+
+
+// public class Stoic {
+//   string abu="goddess";
+
+//   public void pers(){
+//     Console.WriteLine(abu);
+//   }
+
+//   public void pers2(){
+//     Console.WriteLine("a born " + abu);
+//   }
+
+// }
+
+
+class Abi{
+    static void Main(string [] args){
+        Console.WriteLine("added value is");
+        Beaut her = new Beaut();
+        her.g();
+        her.booboo();
+
+    }
+}
+
+public class Beaut{
+    int a=20;
+    int b=90;
+    int c;
+
+    public Beaut(){
+        c = a + b;
+    }
+ public void g(){
+        Console.WriteLine(c);
+        if (c>100){
+          Console.WriteLine("its greater than 100");
+        }
+        else{
+          Console.WriteLine("its less than 100");
+        }
+        
+    }
+
+public void booboo(){
+    do{
+        Console.WriteLine(c);
+        c++;
+    }
+    while(c<120);
+}
+}
+
+
+
+
+
