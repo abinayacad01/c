864f3e60e54db7ef36eca62d0447160900dc425a
abinayacad01 <abinayakumarcad01@gmail.com>
2024-02-22 17:47:32 +0530

    modified

---
+++ namespace/Program.cs
@@ -0,0 +1,8 @@
+﻿namespace NAMESPACE{
+    public class test{
+        public static void Main(string[] args){
+            nithya n= new nithya();
+            n.Her();
+        }
+    }
+}
---
+++ namespace/blahblah/nithya.cs
@@ -0,0 +1,8 @@
+namespace NAMESPACE
+{
+ public class nithya{
+    public void Her(){
+            Console.WriteLine("i am her sister");
+    }
+  }   
+}
