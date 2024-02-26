974716a7eeeea074472ee3736ddd87683bebbdf3
abinayacad01 <abinayakumarcad01@gmail.com>
2024-02-26 18:08:59 +0530

    modified

---
+++ inheritance task/brightservices.cs
@@ -0,0 +1,8 @@
+public class Brightskills{
+    public void services(){
+        Console.WriteLine("This is bright skill");
+        Console.WriteLine("we are providing two course packages");
+        Console.WriteLine("one is cad and another one is ai");
+        Console.WriteLine("which one ");
+    }
+}
